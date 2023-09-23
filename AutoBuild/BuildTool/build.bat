2a883a5e59cb2c653b2125660286be75c7d5c299
hoangminh1789 <hoangminh1789@gmail.com>
2023-09-23 20:32:43 +0700

    WIP

--- AutoBuild/BuildTool/build.bat
+++ AutoBuild/BuildTool/build.bat
@@ -4,14 +4,18 @@ ECHO [[== Load build.bat
 SETLOCAL enableDelayedExpansion
 
 CALL %~dp0config.bat
+ECHO.
 
+SET BUILD_DATE=%date:~10,4%%date:~7,2%%date:~4,2%_%time:~0,2%%time:~3,2%
+SET OUTPUT_DIR=%WORKSPACE%/%BUILD_DATE%
+SET LOG_PATH=%OUTPUT_DIR%/log.txt
+SET OUTPUT_PATH=%OUTPUT_DIR%/AutoBuild/Test.exe
 
-SET LOG_PATH=%WORKSPACE%/log.txt
-SET OUTPUT_PATH=%WORKSPACE%/AutoBuild/Test.exe
-
-ECHO PROJECT_PATH: '%PROJECT_PATH%'
-ECHO Unity path: '%UNITY_PATH%'
-
+ECHO - BUILD_DATE: '%BUILD_DATE%'
+ECHO - OUTPUT_DIR: '%OUTPUT_DIR%'
+ECHO - PROJECT_DIR: '%PROJECT_DIR%'
+ECHO - UNITY_PATH: '%UNITY_PATH%'
+ECHO - LOG_PATH: '%LOG_PATH%'
 :loop
 if [%1]==[] goto end_loop
 if [%1]==[-p] goto case_profile
@@ -34,26 +38,29 @@ echo Version: '%version%'
 ECHO Profile path: '%profilePath%'
 
 if exist %profilePath% (
-	CALL %profilePath% :global_config config
-	ECHO Config: '!config!'
+	CALL %profilePath% :global_config globalConfig
+	ECHO Global Config: '!globalConfig!'
+	ECHO.
 ) else (
 	ECHO Load profile '%profilePath%' is failed
 	EXIT 0
 )
 
 ECHO - Starting building process
+ECHO.
 
 "%UNITY_PATH%" ^
 -quit ^
 -batchmode ^
--projectPath "%PROJECT_PATH%" ^
+-projectPath "%PROJECT_DIR%" ^
 -executeMethod Builder.Execute ^
 -buildTarget Win32 ^
 -logFile "%LOG_PATH%" ^
 -outputPath "%OUTPUT_PATH%" ^
 -versionCode %version% ^
-%config%
+%globalConfig%
 
+ECHO.
 ECHO - Finish build
 
 CALL %BUILD_TOOL%utility.bat :validate_file
--- AutoBuild/BuildTool/config.bat
+++ AutoBuild/BuildTool/config.bat
@@ -1,13 +1,13 @@
 ECHO.
 ECHO [[== Execute config.bat
 SET BUILD_TOOL=%~dp0
-SET PROJECT_PATH=%~dp0..\
+SET PROJECT_DIR=%~dp0..\
 SET UNITY_PATH=C:\Program Files\Unity\Hub\Editor\2022.3.7f1\Editor\Unity.exe
 SET WORKSPACE=C:\wkspaces
 
 ECHO Variables in config.bat
 ECHO BUILD_TOOL: '%BUILD_TOOL%'
-ECHO PROJECT_PATH: '%PROJECT_PATH%'
+ECHO PROJECT_DIR: '%PROJECT_DIR%'
 ECHO UNITY_PATH: '%UNITY_PATH%'
 ECHO WORKSPACE: '%WORKSPACE%'
 
